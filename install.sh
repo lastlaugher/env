f346092c3a770a8a00dafcb75ea19397a9bcf66d
Jihoon Kim <jihoonk@qti.qualcomm.com>
2020-08-27 18:51:44 +0900

    Git clone Vundle

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ cat .bashrc >> ~/.bashrc
 cp -a .vim ~/.vim
 cp .vimrc ~/
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 
